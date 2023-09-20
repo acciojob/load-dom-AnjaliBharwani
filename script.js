4b8a17bd1e42d95a991a1b00d248fdb62544077e
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-09-20 12:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    // Clear the existing content of the <body> element
+    document.body.innerHTML = '';
+
+    // Create a text node with the desired content
+    var textNode = document.createTextNode('DOM load success');
+
+    // Append the text node to the <body> element
+    document.body.appendChild(textNode);
+  }, 4000);
+});
